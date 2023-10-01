caab37343e4ff175c7f1a056da475d8d1afd95bb
Suzan Zeytcioglu <suzan.zeytcioglu.0@gmail.com>
2023-10-01 22:04:07 +0300

    node.js

---
+++ Backend Web Development/2. Node.js/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname"
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+// const superheroes = require("superheroes")
+const name = superheroes.random();
+console.log(`My name is ${name}.`)
